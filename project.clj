18b2af8acfa5ba84e0b556b28d4aa3848cd4f9e6
Jeremiah Coyle <jcoyle@gmail.com>
2024-02-13 00:01:50 -0700

    Update versions

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.paintparty/typetag "0.1.0"
+(defproject io.github.paintparty/typetag "0.2.0"
   :description "Clojure(Script) utility for discerning types of values."
   :url "https://github.com/paintparty/typetag"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :repl-options {:init-ns typetag.core})
