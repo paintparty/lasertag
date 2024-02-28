002e9770b713d76b5c66e43817c97ad61106d9ca
Jeremiah Coyle <jcoyle@gmail.com>
2024-02-27 18:21:59 -0700

    Bumb minor version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.paintparty/typetag "0.2.0"
+(defproject io.github.paintparty/typetag "0.3.0"
   :description "Clojure(Script) utility for discerning types of values."
   :url "https://github.com/paintparty/typetag"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
