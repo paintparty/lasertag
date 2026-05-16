75b765cf948239a92cb6ed7a74855db713cceec2
Jeremiah Coyle <jcoyle@gmail.com>
2026-05-16 01:20:59 -0600

    Add some more candidates to cljs

--- src/lasertag/jsi/codegen.cljs
+++ src/lasertag/jsi/codegen.cljs
@@ -38,87 +38,106 @@
   (with-datafied-demos
     {
 
-     ;; ;; ---------------------------------------------------------------------
-     ;; ;; Primatives and ClojureScript classes
-     ;; ;; ---------------------------------------------------------------------
-     ["cljs.core/Keyword" :keyword]         ":foo"
-     ["js/String" :string]                  "\"foo\""
-       ;; ["cljs.core/Symbol" :symbol]           "(symbol foo)"
-     ["js/Boolean" :boolean]                "true"
-     ["js/Number" :number]                  "21"
-     ["nil" :nil]                           "nil"
-     ["cljs.core/UUID" :uuid ]               "(uuid \"4fe5d828-6444-11e8-8222-720007e40350\")"
-     ["js/RegExp" :regex]                   "(js/RegExp. \"hello\")"
-     ["cljs.core/PersistentArrayMap" :map]  "{1 2 3 4}"
-     ["cljs.core/PersistentHashMap" :map]   "(hash-map 1 2 3 4)"
-     ["cljs.core/LazySeq" :seq]             "(map inc [1 2 3])"
-     ["cljs.core/PersistentVector" :vector] "[1 2 3]"
-     ["cljs.core/PersistentHashSet" :set]   "#{1 2 3}"
-     ["cljs.core/Subvec" :vector]           "(subvec [1 2 3 4 5] 1 3)"
-     ["cljs.core/Cons" :seq]                "(cons 1 (list 2 3))"
-     ["cljs.core/IntegerRange" :seq]        "(range 3)"
-     ["cljs.core/Range" :seq]               "(range 0 1.0 0.1)"
-     ["cljs.core/Repeat" :seq]              "(repeat 2 :a)"
-     ["cljs.core/List" :list]               "(list 1 2 3)"
-     ["cljs.core/EmptyList" :list]          "(list)"
-     ["cljs.core/PersistentTreeMap" :map]   "(sorted-map 1 2 3 4)"
-     ["cljs.core/PersistentTreeSet" :set]   "(sorted-set 1 2 3)"
-     ["cljs.core/TransientArrayMap" :map]   "(transient (array-map 1 2 3 4))"
-     ["cljs.core/TransientHashMap" :map]    "(transient (hash-map 1 2 3 4))"
-     ["cljs.core/TransientVector" :vector]  "(transient [1 2 3])"
-     ["cljs.core/TransientHashSet" :set]    "(transient #{1 2 3})"
-     ["cljs.core/PersistentQueue" :queue]   "cljs.core.PersistentQueue.EMPTY"
-     ["cljs.core/MapEntry" :vector]         "(-> {:a 1} first)"
-     ["cljs.core/MultiFn" :function]        "(do (defmulti different-behavior (fn [x] (:x-type x))) different-behavior)"
+     ;; ---------------------------------------------------------------------
+     ;; Primatives and ClojureScript classes
+     ;; ---------------------------------------------------------------------
+     ["cljs.core/Keyword" :keyword]           ":foo"
+     ["js/String" :string]                    "\"foo\""
+     ;; ["cljs.core/Symbol" :symbol]           "(symbol foo)"
+     ["js/Boolean" :boolean]                  "true"
+     ["js/Number" :number]                    "21"
+     ["nil" :nil]                             "nil"
+     ["cljs.core/UUID" :uuid ]                "(uuid \"4fe5d828-6444-11e8-8222-720007e40350\")"
+     ["js/RegExp" :regex]                     "(js/RegExp. \"hello\")"
+     ["cljs.core/PersistentArrayMap" :map]    "{1 2 3 4}"
+     ["cljs.core/PersistentHashMap" :map]     "(hash-map 1 2 3 4)"
+     ["cljs.core/LazySeq" :seq]               "(map inc [1 2 3])"
+     ["cljs.core/PersistentVector" :vector]   "[1 2 3]"
+     ["cljs.core/PersistentHashSet" :set]     "#{1 2 3}"
+     ["cljs.core/Subvec" :vector]             "(subvec [1 2 3 4 5] 1 3)"
+     ["cljs.core/Cons" :seq]                  "(cons 1 (list 2 3))"
+     ["cljs.core/IntegerRange" :seq]          "(range 3)"
+     ["cljs.core/Range" :seq]                 "(range 0 1.0 0.1)"
+     ["cljs.core/Repeat" :seq]                "(repeat 2 :a)"
+     ["cljs.core/List" :list]                 "(list 1 2 3)"
+     ["cljs.core/EmptyList" :list]            "(list)"
+     ["cljs.core/PersistentTreeMap" :map]     "(sorted-map 1 2 3 4)"
+     ["cljs.core/PersistentTreeSet" :set]     "(sorted-set 1 2 3)"
+     ["cljs.core/TransientArrayMap" :map]     "(transient (array-map 1 2 3 4))"
+     ["cljs.core/TransientHashMap" :map]      "(transient (hash-map 1 2 3 4))"
+     ["cljs.core/TransientVector" :vector]    "(transient [1 2 3])"
+     ["cljs.core/TransientHashSet" :set]      "(transient #{1 2 3})"
+     ["cljs.core/PersistentQueue" :queue]     "cljs.core.PersistentQueue.EMPTY"
+     ["cljs.core/MapEntry" :vector]           "(-> {:a 1} first)"
+     ["cljs.core/MultiFn" :function]          "(do (defmulti different-behavior (fn [x] (:x-type x))) different-behavior)"
      
+
+     ;; TODO - Check and add
+    ;;  ["cljs.core/ArrayNodeSeq" :seq]          "(seq (apply hash-map (interleave (range 100) (range 100))))"
+    ;;  ["cljs.core/ChunkedCons" :seq]           "(chunk-cons (chunk (doto (chunk-buffer 1) (chunk-append 0))) nil)"
+    ;;  ["cljs.core/ChunkedSeq" :seq]            "(seq (vec (range 33)))"
+    ;;  ["cljs.core/Cycle" :seq]                 "(cycle [1 2 3])"
+    ;;  ["cljs.core/ES6IteratorSeq" :seq]        "(es6-iterator-seq (.values #js [1 2 3]))"
+    ;;  ["cljs.core/IndexedSeq" :seq]            "(seq [1 2 3])"
+    ;;  ["cljs.core/IntegerRangeChunk" :chunk]   "(chunk-first (seq (range 32)))"
+    ;;  ["cljs.core/Iterate" :seq]               "(iterate inc 0)"
+    ;;  ["cljs.core/KeySeq" :seq]                "(keys {:a 1 :b 2})"
+    ;;  ["cljs.core/NodeSeq" :seq]               "(seq (apply hash-map (range 20)))"
+    ;;  ["cljs.core/PersistentArrayMapSeq" :seq] "(seq (array-map :a 1 :b 2))"
+    ;;  ["cljs.core/PersistentQueueSeq" :seq]    "(seq (conj cljs.core.PersistentQueue.EMPTY 1 2 3))"
+    ;;  ["cljs.core/PersistentTreeMapSeq" :seq]  "(seq (sorted-map :a 1 :b 2))"
+    ;;  ["cljs.core/RSeq" :seq]                  "(rseq [1 2 3])"
+    ;;  ["cljs.core/ValSeq" :seq]                "(vals {:a 1 :b 2})"
+
+
      ;; ---------------------------------------------------------------------
      ;; JS Built-ins
      ;; ---------------------------------------------------------------------
      
      ;; Reference types
-     ["js/Promise" :promise]                "(new js/Promise (fn [x] x))"
+     ["js/Promise" :promise]                  "(new js/Promise (fn [x] x))"
 
      ;; Error objects
-     ["js/AggregateError" :throwable]       "(new js/AggregateError (array (new js/Error \"some error\")) \"Hello\")",
-     ["js/EvalError" :throwable]            "(new js/EvalError)",
-     ["js/RangeError" :throwable]           "(new js/RangeError)",
-     ["js/ReferenceError" :throwable]       "(new js/ReferenceError)",
-     ["js/SyntaxError" :throwable]          "(new js/SyntaxError)",
-     ["js/TypeError" :throwable]            "(new js/TypeError)",
-     ["js/URIError" :throwable]             "(new js/URIError)",
-     ["js/Error" :throwable]                "(new js/Error)"
+     ["js/AggregateError" :throwable]         "(new js/AggregateError (array (new js/Error \"some error\")) \"Hello\")",
+     ["js/EvalError" :throwable]              "(new js/EvalError)",
+     ["js/RangeError" :throwable]             "(new js/RangeError)",
+     ["js/ReferenceError" :throwable]         "(new js/ReferenceError)",
+     ["js/SyntaxError" :throwable]            "(new js/SyntaxError)",
+     ["js/TypeError" :throwable]              "(new js/TypeError)",
+     ["js/URIError" :throwable]               "(new js/URIError)",
+     ["js/Error" :throwable]                  "(new js/Error)"
 
      ;; Keyed collections 
-     ["js/Map" :map]                        "(new js/Map (array (array \"a\", 1), (array \"b\", 2)))",
-     ["js/Set" :set]                        "(new js/Set (array 1 2))",
-     ["js/WeakMap" :map]                    "(let [wm (js/WeakMap.) o (js-obj :a 1)] (.set wm o 100))",
-     ["js/WeakSet" :set]                    "(new js/Set (array 1 2))"
+     ["js/Map" :map]                          "(new js/Map (array (array \"a\", 1), (array \"b\", 2)))",
+     ["js/Set" :set]                          "(new js/Set (array 1 2))",
+     ["js/WeakMap" :map]                      "(let [wm (js/WeakMap.) o (js-obj :a 1)] (.set wm o 100))",
+     ["js/WeakSet" :set]                      "(new js/Set (array 1 2))"
 
      ;; Indexed collections 
-     ["js/BigInt64Array" :array]            "(new js/BigInt64Array 3)",
-     ["js/BigUint64Array" :array]           "(new js/BigUint64Array 3)",
-     ["js/Int32Array" :array]               "(new js/Int32Array (array 1 2 3))",
-     ["js/Uint16Array" :array]              "(new js/Uint16Array (array 1 2 3))",
-     ["js/Int8Array" :array]                "(new js/Int8Array (array 1 2 3))",
-     ["js/Uint32Array" :array]              "(new js/Uint32Array (array 1 2 3))",
-     ["js/Array" :array]                    "(new js/Array 1 2 3)",
-     ["js/Uint8Array" :array]               "(new js/Uint8Array (array 1 2 3))",
-     ["js/Int16Array" :array]               "(new js/Int16Array (array 1 2 3))",
-     ["js/Uint8ClampedArray" :array]        "(new js/Uint8ClampedArray (array 1 2 3))",
-     ["js/Float32Array" :array]             "(new js/Float32Array (array 1 2 3))",
-     ["js/Float64Array" :array]             "(new js/Float64Array (array 1 2 3))"
+     ["js/BigInt64Array" :array]              "(new js/BigInt64Array 3)",
+     ["js/BigUint64Array" :array]             "(new js/BigUint64Array 3)",
+     ["js/Int32Array" :array]                 "(new js/Int32Array (array 1 2 3))",
+     ["js/Uint16Array" :array]                "(new js/Uint16Array (array 1 2 3))",
+     ["js/Int8Array" :array]                  "(new js/Int8Array (array 1 2 3))",
+     ["js/Uint32Array" :array]                "(new js/Uint32Array (array 1 2 3))",
+     ["js/Array" :array]                      "(new js/Array 1 2 3)",
+     ["js/Uint8Array" :array]                 "(new js/Uint8Array (array 1 2 3))",
+     ["js/Int16Array" :array]                 "(new js/Int16Array (array 1 2 3))",
+     ["js/Uint8ClampedArray" :array]          "(new js/Uint8ClampedArray (array 1 2 3))",
+     ["js/Float32Array" :array]               "(new js/Float32Array (array 1 2 3))",
+     ["js/Float64Array" :array]               "(new js/Float64Array (array 1 2 3))"
 
      ;; Internalization
-     ["js/Intl.DateTimeFormat" :intl]       "(new js/Intl.DateTimeFormat \"en-US\")",
-     ["js/Intl.RelativeTimeFormat" :intl]   "(new js/Intl.RelativeTimeFormat \"en\" (js-obj :style \"short\"))",
-     ["js/Intl.Locale" :intl]               "(new js/Intl.Locale \"ko\" (js-obj :script \"Kore\", :region \"KR\", :hourCycle \"h23\", :calendar \"gregory\"))",
-     ["js/Intl.Collator" :intl]             "(new js/Intl.Collator \"sv\")",
-     ["js/Intl.DisplayNames" :intl]         "(new js/Intl.DisplayNames (clj->js [\"en\"]) (js-obj \"type\" \"region\"))",
-     ["js/Intl.PluralRules" :intl]          "(new js/Intl.PluralRules \"en-US\")",
+     ["js/Intl.DateTimeFormat" :intl]         "(new js/Intl.DateTimeFormat \"en-US\")",
+     ["js/Intl.RelativeTimeFormat" :intl]     "(new js/Intl.RelativeTimeFormat \"en\" (js-obj :style \"short\"))",
+     ["js/Intl.Locale" :intl]                 "(new js/Intl.Locale \"ko\" (js-obj :script \"Kore\", :region \"KR\", :hourCycle \"h23\", :calendar \"gregory\"))",
+     ["js/Intl.Collator" :intl]               "(new js/Intl.Collator \"sv\")",
+     ["js/Intl.DisplayNames" :intl]           "(new js/Intl.DisplayNames (clj->js [\"en\"]) (js-obj \"type\" \"region\"))",
+     ["js/Intl.PluralRules" :intl]            "(new js/Intl.PluralRules \"en-US\")",
      ;; ["js/Intl" :intl]                       nil,
-     ["js/Intl.NumberFormat" :intl]         "(new js/Intl.NumberFormat \"de-DE\" (js-obj :style \"currency\", :currency \"EUR\"))",
-     ["js/Intl.Segmenter" :intl]            "(new js/Intl.Segmenter \"fr\" (js-obj :granularity \"word\"))",
-     ["js/Intl.ListFormat" :intl]           "(new js/Intl.ListFormat \"en-GB\" (js-obj :style \"long\", :type \"conjunction\"))"
+     ["js/Intl.NumberFormat" :intl]           "(new js/Intl.NumberFormat \"de-DE\" (js-obj :style \"currency\", :currency \"EUR\"))",
+     ["js/Intl.Segmenter" :intl]              "(new js/Intl.Segmenter \"fr\" (js-obj :granularity \"word\"))",
+     ["js/Intl.ListFormat" :intl]             "(new js/Intl.ListFormat \"en-GB\" (js-obj :style \"long\", :type \"conjunction\"))"
      }))
 
 (def by-class
